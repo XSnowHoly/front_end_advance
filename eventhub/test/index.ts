aefcccdfb596b7da07b99626a9f188491275a9bc
xsnowholy <495904127@qq.com>
2019-09-26 22:37:19 +0800

    feat: 基本完成eventhub功能和测试用例

--- eventhub/test/index.ts
+++ eventhub/test/index.ts
@@ -1,13 +1,41 @@
 import EventHub from '../src/index';
 
-const eventhub = new EventHub();
+const test1 = message => {
+   const eventhub = new EventHub();
+   console.assert(eventhub instanceof Object === true, 'EventHub是一个对象');
+   console.log(message)
+}
+   
 
-console.assert(eventhub instanceof Object === true, 'EventHub是一个对象');
+const test2 = message => {
+   const eventhub = new EventHub();
+   let called = false;
+   eventhub.on('xxx', (params) => {
+      called = true;
+      console.assert(params === '呵呵')
+   });
+   eventhub.emit('xxx', '呵呵');
+   setTimeout(() => {
+      console.assert(called === true);
+      console.log(message)
+   })
+}
 
-let called = false;
-eventhub.on('xxx', ()=> {
-   called = true;
-   console.log('called: ' + called);
-});
+const test3 = message => {
+   const eventhub = new EventHub();
+   let called2 = false;
+   const fn1 = () => {
+      called2 = true;
+   }
+   eventhub.on('xxx', fn1);
+   eventhub.off('xxx', fn1);
+   eventhub.emit('xxx');
+   setTimeout(() => {
+      console.assert(called2 === false);
+      console.log(message)
+   }, 2000)
+}
 
-eventhub.emit('xxx');
+test1('EventHub可以创建一个对象');
+test2('EventHub可以on订阅事件和emit触发事件');
+test3('EventHub可以off取消一个订阅事件');
