429d9ec1d612c829f98e1b90e2054eb3b28791fb
xsnowholy <495904127@qq.com>
2019-10-13 21:28:27 +0800

    feat: 新增复制基本数据类型和普通对象

--- deepCopy/src/index.js
+++ deepCopy/src/index.js
@@ -1,5 +1,19 @@
-function deepClone() {
-
+function deepClone(source) {
+    if(null === source) {
+        return null;
+    }
+    if(typeof source !== 'object') {
+        return source;
+    }
+    if(source instanceof Object) {
+        const dist = new Object();
+        for (const key in source) {
+            if (source.hasOwnProperty(key)) {
+                dist[key] = deepClone(source[key]);
+            }
+        }
+        return dist;
+    }
 }
 
 module.exports = deepClone;
--- deepCopy/test/index.js
+++ deepCopy/test/index.js
@@ -10,4 +10,34 @@ describe('deepClone', () => {
     it('是一个函数', () => {
         assert.isFunction(deepClone)
     });
+    it('能复制基本类型', () => {
+        let num = 123;
+        let numCopy = deepClone(num);
+        let str = '123';
+        let strCopy = deepClone(str);
+        let empty = undefined;
+        let emptyCopy = deepClone(undefined);
+        let nullVal = null;
+        let nullValCopy = deepClone(nullVal);
+        let bool = true;
+        let boolCopy = deepClone(bool);
+        let sym = Symbol();
+        let sym2 = deepClone(sym);
+        assert(num === numCopy);
+        assert(str === strCopy);
+        assert(empty === emptyCopy);
+        assert(nullVal === nullValCopy);
+        assert(bool === boolCopy);
+        assert(sym === sym2);
+    });
+    describe('对象', () => {
+        it('能够复制普通对象', () => {
+            let a = { name: 'cxk', skill: { name: '篮球' } };
+            let b = deepClone(a);
+            assert(a !== b);
+            assert(a.name === b.name);
+            assert(a.skill !== b.skill);
+            assert(a.skill.name === b.skill.name);
+        });
+    });
 });
