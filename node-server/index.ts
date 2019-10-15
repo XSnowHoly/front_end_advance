60c065ae13a5dc029b07a3707cf836ca3bc66d39
xsnowholy <495904127@qq.com>
2019-10-15 22:41:02 +0800

    feat: 测试简单静态服务器的响应请求

--- node-server/index.ts
+++ node-server/index.ts
@@ -1,25 +1,32 @@
 import * as http from "http";
+import { readFile } from "fs";
+import { resolve, relative } from "path";
+import * as url from "url";
 
 const server = http.createServer();
+const publicDir = resolve(__dirname, 'public')
+console.log('public', publicDir);
+
 
 server.on('request', (request: http.IncomingMessage, response: http.ServerResponse) => {
-    // console.log(http.IncomingMessage)
-    console.log('request.method');
-    console.log(request.method);
-    console.log('request.url');
-    console.log(request.url);
-    console.log('request.headers');
-    console.log(request.headers)
+    const { url: path } = request;
+    const { setHeader } = response
+    const { pathname } = url.parse(path);
+    let filename = pathname.substr(1);
+    if (filename === '') {
+        filename = 'index.html'
+    }
+    readFile(resolve(publicDir, filename), (error, data) => {
+        if (error) {
+            console.log(error);
+            if (error.errno === -4058) {
+                response.statusCode = 404;
+                response.end('404')
+            }
+        } else {
+            response.end(data.toString());
+        }
 
-    const array = [];
-    request.on('data', (chunk) => {
-        array.push(chunk)
-    })
-    request.on('end', () => {
-        const body = Buffer.concat(array).toString();
-        console.log('body');
-        console.log(body);
-        response.end('hi')
     })
 })
 
