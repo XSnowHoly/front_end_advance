8ec6a6f0874b46d234f03f5be0d089395d8b2f77
xsnowholy <wblai@foxmail.com>
2019-10-24 17:32:31 +0800

    refactor: (深拷贝): 优化代码
      优化代码逻辑实现
      优化类型的查找方式，改为表驱动查询
     编写相应注释，增加可读性

--- deepCopy/src/index.js
+++ deepCopy/src/index.js
@@ -1,37 +1,38 @@
 function deepClone(source) {
-    let cache = [];
-    return baseClone(source)
+    let cache = [];         // 创建一个缓存对象的数组
+    const typeInitMap = {   // 支持初始化的类型map
+        object: () => ({}),
+        array: () => [],
+        function: fn => (...arg) => fn(...arg),
+        date: date => new Date(date),
+        regexp: reg => new RegExp(reg.source, reg.flags)
+    }
+
+    function getType(data) {
+        if (arguments.length === 0) return new Error('没有接收到参数')
+        var typeStr = Object.prototype.toString.call(data)
+        return typeStr.match(/\[object (.*?)\]/)[1].toLowerCase()
+    }
 
     function baseClone(source) {
-        if (null === source) return null;
-        if (source instanceof Object) {
-            let dist;
-            let cacheDist = findCache(source);  // 查找缓存中是否存在对象，解决环状对象问题
-            if (cacheDist) return cacheDist;
-            if (source instanceof Array) {
-                dist = new Array();
-            } else if (source instanceof Function) {
-                dist = function () {
-                    return source.call(this, ...arguments);
-                }
-            } else if (source instanceof RegExp) {
-                dist = new RegExp(source.source, source.flags);
-            } else if (source instanceof Date) {
-                dist = new Date(source);
-            } else {
-                dist = new Object();
-            }
-            cache.push([source, dist])
-            for (const key in source) {
+        const sourceType = getType(source);
+        if (sourceType in typeInitMap) {
+            let copySource;
+            let cacheSource = findCache(source); // 查找缓存中是否存在对象,解决环状对象问题
+            if (cacheSource) return cacheSource;   // 如果目标对象存在缓存中,则直接返回缓存中的复制的对象
+            copySource = typeInitMap[sourceType](source);
+            cache.push([source, copySource])
+            for (const key in source) {            // 递归遍历对象,子元素为对象时继续执行深拷贝
                 if (source.hasOwnProperty(key)) {
-                    dist[key] = baseClone(source[key]);
+                    copySource[key] = baseClone(source[key]);
                 }
             }
-            return dist;
+            return copySource;
         }
         return source;
     }
 
+    // 查找缓存中是否存在目标对象, 如果存在目标对象, 返回复制的目标对象
     function findCache(source) {
         for (let i = 0; i < cache.length; i++) {
             if (cache[i][0] === source) {
@@ -40,6 +41,8 @@ function deepClone(source) {
         }
         return undefined;
     }
+
+    return baseClone(source)
 }
 
 
